9f7529f8e75e678d80a1c45e7c30d305001964e3
Christian Speich <kleinweby@kleinweby.de>
2009-05-26 12:16:26 +0200

    Adding Copyright Infos

--- Sources/PlugInRegistry.h
+++ Sources/PlugInRegistry.h
@@ -1,10 +1,24 @@
-//
-//  PlugInRegistry.h
-//  XAMPP Control
-//
-//  Created by Christian Speich on 20.04.09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/*
+ 
+ The PlugIn Framework
+ Copyright (C) 2009 by Christian Speich <kleinweby@kleinweby.de>
+ 
+ This file is part of the PlugIn Framework.
+ 
+ The PlugIn Framework is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ The PlugIn Framework is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with the PlugIn Framework.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
 
 #import <Cocoa/Cocoa.h>
 
